252829f8858daca3ba771c56cd5d3211a3fc14d7
wellsvang <wangtulee@gmail.com>
2026-03-30 14:40:47 +0700

    first update

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -60,8 +60,8 @@ const Hero = () => {
                         I'm a
                         <span className='slide'>
                             <span className='wrapper'>
-                               {words.map((word) => (
-                                <span key={word.text} className='flex items-center md:gap-3 gap-1 pb-2'>
+                               {words.map((word, index) => (
+                                <span key={index} className='flex items-center md:gap-3 gap-1 pb-2'>
                                     <img 
                                         src={word.imgPath}
                                         alt={word.text}
